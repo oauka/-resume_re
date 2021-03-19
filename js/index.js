3c0c4c6e48880d261c729ff50f767eb4ca922748
oauka <77268885+oauka@users.noreply.github.com>
2021-03-19 14:32:21 +0900

    ver 1.2

--- js/index.js
+++ js/index.js
@@ -1,17 +1,5 @@
-// $('#header .open').on('click', function(){
-//     $(this).removeClass('on')
-//     $(this).next().next().next().next().addClass('on')
-//     $(this).next().addClass('on')
-// })
-
-// $('#header .close').on('click', function(){
-//     $(this).removeClass('on')
-//     $(this).prev().prev().prev().prev().addClass('on')
-//     $(this).prev().prev().prev().removeClass('on')
-//     $(this).prev().find('.depth1 > li').removeClass('on')
-// })
-
 $('.depth1 > li').on('click', function(e){
+    e.stopPropagation()
     var index = $(this).index()
     if (index===1) {
         e.preventDefault()
@@ -27,6 +15,9 @@ $('.depth1 > li').on('click', function(e){
 $('.depth2 > li').on('click', function(e){
 e.stopPropagation()
 })
+$('.depth2').on('click', function(e){
+    e.stopPropagation()
+    })
 
 // 메인슬라이드 : 슬릭슬라이더 연결
 $('.slide-group').slick({
@@ -44,33 +35,3 @@ $('.slide-group').slick({
         }
     }]
 })
-
-/////////////////////////////////////////////////////////////////////////////////////
-
-var portNear = $('#portfolio').offset().top
-var meNear = $('#me').offset().top
-var contactNear = $('#contactme').offset().top  
-$('#nav .depth1 > li').on('click', function(e){
-    e.preventDefault()
-    var num = $(this).index()
-    switch(num) {
-        case 0 : $('html').stop().animate({ scrollTop:0 }, 500); break;
-        case 1 : $('html').stop().animate({ scrollTop:portNear }, 500); break;
-        case 2 : $('html').stop().animate({ scrollTop:contactNear }, 500); break;
-        default : break;
-    }
-}) 
-
-// $('.section').on('mousewheel', function(e, delta){
-//     if (delta>0) {
-//         var prev = $(this).prev().offset().top
-//         $('html').stop().animate({
-//             scrollTop:prev
-//         }, 500, 'linear')
-//     } else if (delta<0) {
-//         var next = $(this).next().offset().top
-//         $('html').stop().animate({
-//             scrollTop:next
-//         }, 500, 'linear')
-//     }
-// })
